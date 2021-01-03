018dce8b5058ca8f1250b800ed9b651ff7f87e41
Marcelo <mrclgst10@gmail.com>
2021-01-03 12:16:14 -0300

    refactor: move AuthenticationSpy to its on file

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,21 +1,8 @@
 import React from 'react'
 import faker from 'faker'
 import { render, RenderResult, fireEvent, cleanup } from '@testing-library/react'
-import { ValidationSpy } from '@/presentation/test'
+import { ValidationSpy, AuthenticationSpy } from '@/presentation/test'
 import Login from './login'
-import { Authentication, AuthenticationParams } from '@/domain/usecases'
-import { AccountModel } from '@/domain/models'
-import { mockAccountModel } from '@/domain/test'
-
-class AuthenticationSpy implements Authentication {
-  account = mockAccountModel()
-  params: AuthenticationParams
-
-  async auth (params: AuthenticationParams): Promise<AccountModel> {
-    this.params = params
-    return await Promise.resolve(this.account)
-  }
-}
 
 type SutTypes = {
   sut: RenderResult
